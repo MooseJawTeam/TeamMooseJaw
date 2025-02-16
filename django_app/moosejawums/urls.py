0e359e19aa36b38c7ff0922f4506d447adf1fe36
Christa Ongouya <rc.ongouya242@gmail.com>
2025-02-15 18:07:27 -0600

    updated django app

--- django_app/moosejawums/urls.py
+++ django_app/moosejawums/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path ("ums/", include("ums.urls")),
+    path("ums/", include("ums.urls")),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/ums/', permanent=True)),
 ]
