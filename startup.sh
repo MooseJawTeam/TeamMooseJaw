375ec9ecfae4f9e2be06152b2280a773199033e7
Christa Ongouya <rc.ongouya242@gmail.com>
2025-02-17 18:18:40 -0600

    Try again

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 echo 'Running startup.sh script'
 cd django_app
-pip --root-user-action install -r requirements.txt
+pip install --root-user-action ignore -r requirements.txt
 python manage.py runserver $PORT
