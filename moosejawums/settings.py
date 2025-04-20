c8eb76a93c0c046afa6ed2953b3c0a6ca90a80c1
godvap <125163995+godvap@users.noreply.github.com>
2025-04-20 17:27:02 -0500

    Update settings.py
    Changed back to live database, not dev database

--- moosejawums/settings.py
+++ moosejawums/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'moosejawums.wsgi.application'
 # Use SQLite for development
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mjapp',
+        'HOST': 'moosejawdb.mysql.database.azure.com',
+        'PORT': '3306', 
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'USER': os.environ.get('DB_USER'),
     }
 }
 
